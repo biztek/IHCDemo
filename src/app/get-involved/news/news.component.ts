733c337a8ae699b635c9a3bcbfd64eb8ee757af3
“puttanikhileshreddy” <puttanikhileshreddy@gmail.com>
2018-10-17 21:21:01 +0530

    added 4 messages

---
+++ src/app/get-involved/news/news.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.scss']
+})
+export class NewsComponent implements OnInit {
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
